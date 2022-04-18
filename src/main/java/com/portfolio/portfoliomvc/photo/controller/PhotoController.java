27a7c33f20d901c493c7a210a1a1a59030202c39
Lucas Sandoval <sandovalucas88@gmail.com>
2022-04-18 11:26:32 -0300

    Environment.getProperties() para recuperar el 'storage' donde se van a guardar las fotos

--- src/main/java/com/portfolio/portfoliomvc/photo/controller/PhotoController.java
+++ src/main/java/com/portfolio/portfoliomvc/photo/controller/PhotoController.java
@@ -1,29 +1,23 @@
 package com.portfolio.portfoliomvc.photo.controller;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import javax.imageio.ImageIO;
-
+import org.hibernate.cfg.Environment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,17 +25,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.portfolio.portfoliomvc.photo.entity.Photo;
-import com.portfolio.portfoliomvc.photo.service.IPhotoHQLService;
 import com.portfolio.portfoliomvc.photo.service.IPhotoService;
-import com.portfolio.portfoliomvc.photo.service.impl.PhotoServiceImpl;
 import com.portfolio.portfoliomvc.post.entity.Post;
 import com.portfolio.portfoliomvc.post.service.IPostService;
-import com.portfolio.portfoliomvc.relations.PhotoOfPost;
+import com.portfolio.portfoliomvc.relations.SamplePhoto;
 import com.portfolio.portfoliomvc.relations.service.IPhotoPostService;
+import com.portfolio.portfoliomvc.util.random.RandomFileName;
 
 @Controller
 @RequestMapping("/photo")
@@ -57,6 +49,9 @@ public class PhotoController {
 
 	@Autowired
 	private IPhotoPostService photoPostService;
+	
+	//   src/upload
+	public String volumen = Environment.getProperties().getProperty("portfolio.volumen.url");
 
 	// ==================================================
 	// =================== GET IMAGE ====================
@@ -64,9 +59,11 @@ public class PhotoController {
 
 	@GetMapping("/{nombreFoto:.+}")
 	public ResponseEntity<Resource> getImage(@PathVariable String nombreFoto) {
-		Path rutaArchivo = Paths.get("src/upload").resolve(nombreFoto).toAbsolutePath();
+		log.info("volumen:" + volumen);
+		
+		Path rutaArchivo = Paths.get(volumen).resolve(nombreFoto).toAbsolutePath();
 		Resource archivo = null;
-
+		log.info(rutaArchivo.toString());
 		try {
 			archivo = new UrlResource(rutaArchivo.toUri());
 		} catch (MalformedURLException e) {
@@ -85,33 +82,55 @@ public class PhotoController {
 	// ==================================================
 
 	@PostMapping("/upload")
-	public ResponseEntity<?> upload(@RequestParam("file") MultipartFile file, @RequestParam("id") Long idPost) {
-		Map<String, Object> response = new HashMap<>();
+	public ResponseEntity<?> upload(@RequestParam("file") MultipartFile file, 
+							@RequestParam("id") Long idPost, 
+							@RequestParam("isSample") boolean isSample) {
+		
+		Map<String, Object> response = new HashMap<>(); 
 		Post posteo = this.postService.getPostById(idPost).get();
 		
-		// (1) STEP ONE: retrive the resource from the request(MultipartFile).
 		if (!file.isEmpty()) {
-			
-			String fileName = file.getOriginalFilename();
-			Path filePath = Paths.get("src/upload").resolve(fileName).toAbsolutePath(); //Donde lo va a almacenar.
-
-			// (2) STEP TWO: save the resource to the path specified.
-			saveFile(file, filePath); //Guarda el 'file' en el 'filePath' especificado.
-			
-			// -----------------------------------
-			// (3) STEP THREE: Crea el registro 'Photo' con la URL donde esta guardado.
-			Photo savedPhoto = savePhoto(fileName);
-			// -----------------------------------
-			
-			// (4) STEP FOUR: Crea el registro 'Post' con
-			savePhotoOfPost(idPost, savedPhoto.getId());
-			
-			// -----------------------------------
-
+			Optional<Post> post = postService.getPostById(idPost);
+			if (post.isPresent()) {
+				
+				String nombreArchivo = file.getOriginalFilename();
+				String extension = nombreArchivo.split("\\.")[1];			
+				String fileName = RandomFileName.generateRandomUUID().concat("." + extension);
+				System.out.println(volumen);
+				Path filePath = Paths.get(volumen).resolve(fileName).toAbsolutePath();
+				
+				saveFile(file, filePath); //Guarda 'file' en el volumen
+				Photo savedPhoto = savePhoto(fileName); //Persiste en PHOTO
+				
+				if(isSample) {//Si es true lo guardamos dentro de POST
+					
+					saveSamplePhoto(idPost, savedPhoto.getId()); //Persiste en SAMPLE_PHOTO
+					
+					Post postSaving = post.get();
+					postSaving.setSamplePhoto(savedPhoto);
+					postService.putPost(postSaving);
+					
+				} else if(!isSample) {
+					//Si es falso lo guardamos en PhotoOfPost
+					
+					
+				} else {
+					log.debug("'isSample' is not defined or is null");
+				}
+			} else {
+				response.put("messegeError", "post no encontrado");
+				return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+			}
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
-		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+		
 	}
 	
+	//------------------------------------------------------------------------------------------------------
+	
+	//Almacena 'file' en el servidor
 	private void saveFile(MultipartFile file, Path filePath) {
 		try {
 			Files.copy(file.getInputStream(), filePath);
@@ -121,8 +140,7 @@ public class PhotoController {
 		}
 	}
 	
-	//------------------------------------------------------------------------------------------------------
-	
+	//Persiste en PHOTO
 	private Photo savePhoto(String fileName) {
 		Photo savePhoto = new Photo();
 		savePhoto.setNameResource(fileName);
@@ -130,10 +148,12 @@ public class PhotoController {
 		return savedPhoto;
 	}
 	
-	private PhotoOfPost savePhotoOfPost(Long idPost, Long idPhoto) {
-		PhotoOfPost saveEntity = new PhotoOfPost();
+	//Persiste en SAMPLE_PHOTO
+	private SamplePhoto saveSamplePhoto(Long idPost, Long idPhoto) {
+		SamplePhoto saveEntity = new SamplePhoto();
 		saveEntity.setPostId(idPost);
 		saveEntity.setPhotoId(idPhoto);
 		return photoPostService.guardar(saveEntity);
 	}
+	
 }
